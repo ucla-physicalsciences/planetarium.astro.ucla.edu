6deff98017e6ab9ca5cadf1f028ac5e682760069
hegdesahil <40182986+hegdesahil@users.noreply.github.com>
2023-09-21 07:02:51 -0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://planetarium.astro.ucla.edu",
-  base: "./",
+  base: "/",
   integrations: [
     tailwind(),
     image({
