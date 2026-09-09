d3815e7df72a7d3ae164d4fbb842f578b87176c2
graviyisasleep <gravityisasleep@gmail.com>
2026-09-09 11:43:41 -0700

    font change

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,7 +6,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Futura", ...defaultTheme.fontFamily.sans]
+        sans: ["Jost", ...defaultTheme.fontFamily.sans]
       },
       colors: {
         primary: "var(--color-primary)",
